6b36a338ee3fb00a232b2450764bf4ed9e6629d9
Marc Vincent Francisco <vince@DESKTOP-NDU9RVP.localdomain>
2022-11-03 15:33:57 +0800

    5. Migrations and View Helpers

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,5 @@
 class Movie < ApplicationRecord
+    def flop?
+        total_gross.blank? || total_gross < 250000000
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_02_074116) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_03_065559) do
   create_table "movies", force: :cascade do |t|
     t.string "title"
     t.string "rating"
     t.decimal "total_gross"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
+    t.date "released_on"
   end
 
 end
