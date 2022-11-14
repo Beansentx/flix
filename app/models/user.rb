8a627eefb1e9ee3e5a28a24ebd5624a6d7e76982
Marc Vincent Francisco <vince@DESKTOP-NDU9RVP.localdomain>
2022-11-14 19:16:31 +0800

    23. User Account Model

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  validates :name, presence: true
+  validates :email, presence: true, format: { with: /\A[a-zA-Z0-9.!\#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*\z/ }, 
+            uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 10, allow_blank: true}
+
+end
