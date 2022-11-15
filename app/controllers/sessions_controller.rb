de04ed4f2762ced0d5b27eb9c373724ef21d077e
Beansentx <113097010+Beansentx@users.noreply.github.com>
2022-11-15 17:53:45 +0800

    29. Log out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,5 +15,7 @@ class SessionsController < ApplicationController
     
 
     def destroy
+        session[:user_id] = nil
+        redirect_to movies_url, status: :see_other, notice: "You're signed out"
     end
 end
