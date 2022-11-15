72be4adcc77d4116f8f507be383fce1ee4d64500
Beansentx <113097010+Beansentx@users.noreply.github.com>
2022-11-15 17:33:51 +0800

    27. Authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,16 @@ class SessionsController < ApplicationController
     end
 
     def create
+        @user = User.find_by(email: params[:email])
+        if @user&.authenticate(params[:password])
+            session[:user_id] = @user.id
+            redirect_to @user, notice: "Welcome back, #{@user.name}"
+        else
+            flash.now[:alert] = "Invalid email/password"
+            render :new, status: :unprocessable_entity
+        end
     end 
+    
 
     def destroy
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true, format: { with: /\A[a-zA-Z0-9.!\#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*\z/ }, 
             uniqueness: { case_sensitive: false }
-  validates :password, length: { minimum: 10, allow_blank: true}
+  validates :password, length: { minimum: 0, allow_blank: true}
 
   def gravatar_id
     Digest::MD5::hexdigest(email.downcase)
